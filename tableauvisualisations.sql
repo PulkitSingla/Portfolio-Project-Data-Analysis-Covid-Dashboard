33a58f1058c3b6a19cc489023e1f89ccfd120712
Pulkit Singla <39691665+PulkitSingla@users.noreply.github.com>
2021-08-18 12:24:12 +0530

    Add files via upload

---
+++ tableauvisualisations.sql
@@ -0,0 +1,27 @@
+-- Queries for Tableau visualizations
+
+-- Query 1
+SELECT SUM(new_cases) AS total_cases, SUM(CAST(new_deaths AS int)) AS total_deaths, SUM(CAST(new_deaths AS int))/SUM(new_cases)*100 AS deathpercentage 
+FROM PortfolioProject..CovidDeaths 
+WHERE continent IS NOT NULL 
+ORDER BY 1, 2
+
+-- Query 2
+Select location, SUM(cast(new_deaths as int)) as TotalDeathCount
+From PortfolioProject..CovidDeaths
+Where continent is null 
+and location not in ('World', 'European Union', 'International')
+Group by location
+order by TotalDeathCount desc
+
+-- Query 3
+Select Location, Population, MAX(total_cases) as HighestInfectionCount,  Max((total_cases/population))*100 as PercentPopulationInfected
+From PortfolioProject..CovidDeaths
+Group by Location, Population
+order by PercentPopulationInfected desc
+
+-- Query 4
+Select Location, Population,date, MAX(total_cases) as HighestInfectionCount,  Max((total_cases/population))*100 as PercentPopulationInfected
+From PortfolioProject..CovidDeaths
+Group by Location, Population, date
+order by PercentPopulationInfected desc
